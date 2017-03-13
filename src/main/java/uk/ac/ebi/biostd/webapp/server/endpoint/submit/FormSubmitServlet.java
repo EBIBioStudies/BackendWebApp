6f96c9c53a98cb85597caad4c0b74bfce628b745
Mike <gostev@ebi.ac.uk>
2017-03-13 17:03:14 +0000

    FormSubmitServlet (not tested)

---
+++ src/main/java/uk/ac/ebi/biostd/webapp/server/endpoint/submit/FormSubmitServlet.java
@@ -0,0 +1,162 @@
+package uk.ac.ebi.biostd.webapp.server.endpoint.submit;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.Part;
+
+import org.apache.commons.io.IOUtils;
+
+import uk.ac.ebi.biostd.authz.Session;
+import uk.ac.ebi.biostd.treelog.JSON4Report;
+import uk.ac.ebi.biostd.treelog.LogNode;
+import uk.ac.ebi.biostd.treelog.SimpleLogNode;
+import uk.ac.ebi.biostd.treelog.SubmissionReport;
+import uk.ac.ebi.biostd.util.DataFormat;
+import uk.ac.ebi.biostd.webapp.server.config.BackendConfig;
+import uk.ac.ebi.biostd.webapp.server.endpoint.ServiceServlet;
+import uk.ac.ebi.biostd.webapp.server.mng.SubmissionManager.Operation;
+
+public class FormSubmitServlet extends ServiceServlet
+{
+ private static final long serialVersionUID = 1L;
+
+ public static final String typeParameter = "type";
+ public static final String validateOnlyParameter = "validateOnly";
+ public static final String ignoreAbsentFilesParameter = "ignoreAbsentFiles";
+ public static final String opParameter = "op";
+
+
+ @Override
+ protected void service(HttpServletRequest req, HttpServletResponse response, Session sess) throws ServletException, IOException
+ {
+  if( ! req.getMethod().equalsIgnoreCase("POST") )
+  {
+   respond(response, HttpServletResponse.SC_METHOD_NOT_ALLOWED, "Method "+req.getMethod()+" is not allowed");
+   return;
+  }
+  
+  if( sess == null || sess.isAnonymouns() )
+  {
+   respond(response, HttpServletResponse.SC_UNAUTHORIZED, "FAIL User not logged in");
+   return;
+  }  
+  
+  if( req.getParts().size() != 1 )
+  {
+   respond(response, HttpServletResponse.SC_BAD_REQUEST, "FAIL Invalid number of file parts");
+   return;
+  }
+  
+  Part filePart = req.getParts().iterator().next();
+  
+  String fmtStr = req.getParameter(typeParameter);
+  
+  DataFormat fmt = null;
+  
+  if( fmtStr != null )
+  {
+
+   try
+   {
+    fmt = DataFormat.valueOf(DataFormat.class, fmtStr);
+   }
+   catch(Exception e)
+   {
+    respond(response, HttpServletResponse.SC_BAD_REQUEST, "FAIL Invalid 'type' attribute value: '"+fmtStr+"'");
+    return;
+   }
+  }
+  
+  
+  if( fmt == null )
+  {
+   String fn  = filePart.getSubmittedFileName();
+   
+   if( fn != null )
+   {
+    int pos = fn.lastIndexOf('.');
+    
+    if( pos > 0 )
+    {
+     try
+     {
+      fmt = DataFormat.valueOf(DataFormat.class, fn.substring(pos+1).toLowerCase());
+     }
+     catch(Exception e)
+     {
+     }
+    }
+   }
+  }
+  
+  if( fmt == null )
+  {
+   respond(response, HttpServletResponse.SC_BAD_REQUEST, "FAIL can't determine file type. Define 'type' attribute");
+   return;
+  }
+  
+  
+  Operation act = null;
+
+  String pi = req.getParameter(opParameter);
+
+  if(pi != null && pi.length() > 1)
+  {
+   for( Operation op : Operation.values() )
+   {
+    if( op.name().equalsIgnoreCase(pi) )
+    {
+     act = op;
+     break;
+    }
+   }
+  }
+  
+  if( Operation.DELETE == act || Operation.CHOWN == act || Operation.REMOVE == act || Operation.SETMETA == act || Operation.TRANKLUCATE == act )
+   act=null;
+  
+  if( act == null )
+  {
+   respond(response, HttpServletResponse.SC_BAD_REQUEST, "FAIL operation invalid or not defined");
+   return;
+  }
+  
+
+  byte[] data = IOUtils.toByteArray(filePart.getInputStream());
+
+
+  if( data.length == 0 )
+  {
+   response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+   response.getWriter().print("FAIL Empty file");
+   return;
+  }
+  
+  String vldPrm = req.getParameter(validateOnlyParameter);
+  String ignPrm = req.getParameter(ignoreAbsentFilesParameter);
+  
+  boolean validateOnly = vldPrm != null && ("true".equalsIgnoreCase(vldPrm) || "yes".equalsIgnoreCase(vldPrm) || "1".equals(vldPrm) );
+  boolean ignAbsFiles = ignPrm != null && ("true".equalsIgnoreCase(ignPrm) || "yes".equalsIgnoreCase(ignPrm) || "1".equals(ignPrm) );
+
+  SubmissionReport res = BackendConfig.getServiceManager().getSubmissionManager().createSubmission(data, fmt, "UTF-8", act, sess.getUser(), validateOnly, ignAbsFiles);
+  
+  LogNode topLn = res.getLog();
+  
+  SimpleLogNode.setLevels(topLn);
+  
+  response.setContentType("application/json");
+  
+  JSON4Report.convert(res, response.getWriter());
+ }
+  
+ private void respond( HttpServletResponse response, int code, String msg ) throws IOException
+ {
+  response.setStatus(code);
+  response.setContentType("text/plain");
+  response.getWriter().print(msg);
+ }
+
+}
